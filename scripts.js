0ad2c98a545d6c902419e28b4616d88fe5a38d26
Ahmet Emre EL <117543387+E-AEL@users.noreply.github.com>
2024-01-08 22:50:00 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('login-btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Form gönderimini engelle
-    var loginContainer = document.querySelector('.login-container');
-    loginContainer.style.transform = 'translateY(-100%)'; // Giriş kapsayıcısını yukarıya doğru kaydır
-});
